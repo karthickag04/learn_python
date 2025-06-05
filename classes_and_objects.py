b244b0c42927f17a98fbbebed058415fe6aadb6e
karthickag04 <karthickag04@gmail.com>
2025-06-05 11:13:48 +0530

    added classes and objects session files

---
+++ classes_and_objects.py
@@ -0,0 +1,32 @@
+class OperatorDemoFromPyFile:
+    
+
+    def add(self):
+        value1 = 0
+        value2 = 1
+        result = value1 + value2
+        print(result)
+
+    def subtract(self):
+        value1 = 0
+        value2 = 1
+        result = value1 - value2
+        print(result)
+
+
+class OperatorDemoFromPyFile2:
+    
+
+    def Multiply(self):
+        value1 = 0
+        value2 = 1
+        result = value1 * value2
+        print(result)
+
+    def Division(self):
+        value1 = 0
+        value2 = 1
+        result = value1 / value2
+        print(result)
+
+
